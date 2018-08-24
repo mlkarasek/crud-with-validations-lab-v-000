90975cd3f1d5f441e3a315e0fb54bdc320049ab5
Monica  Karasek <mlkarasek@gmail.com>
2018-08-24 20:49:40 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,7 +29,7 @@ end
   def update
     @song = Song.find(params[:id])
     if @song.update(song_params)
-      redirect_to song_path(@song)
+      redirect_to @song
     else
       render :edit
     end
