8ca89d745ccf638aee2b9a3a6c79c7376b888c00
Monica  Karasek <mlkarasek@gmail.com>
2018-08-24 20:55:59 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,6 @@
 class Song < ActiveRecord::Base
   validates_presence_of :title
-  validates :title, uniqueness: {
-    scope: [:release_year, :artist_name]}
-    validates :release_year, presence: true, if: :released?
-    validates_presence_of :artist_name
+  validates :title, uniqueness: {scope: [:release_year, :artist_name]}
+  validates :release_year, presence: true, if: :released?
+  validates_presence_of :artist_name
 end
